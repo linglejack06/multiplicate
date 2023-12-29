f2b01ac1b5379e47a8b0023280cd938d55908629
Jack Lingle <captainjack0818@gmail.com>
2023-12-29 14:44:56 -0500

    Initial Commit

---
+++ multiplicate/multiplicateApp.swift
@@ -0,0 +1,17 @@
+//
+//  multiplicateApp.swift
+//  multiplicate
+//
+//  Created by Jack Lingle on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct multiplicateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
