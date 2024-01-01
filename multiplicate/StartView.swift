42ef0facd02001dd2268f84772b5a666de62c4a8
Jack Lingle <captainjack0818@gmail.com>
2024-01-01 16:39:19 -0500

    convert random text to correct number values in grid

--- multiplicate/StartView.swift
+++ multiplicate/StartView.swift
@@ -8,19 +8,26 @@
 import SwiftUI
 
 struct StartView: View {
+    var multipliers = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
     var body: some View {
         VStack {
-            ForEach(0..<3) { _ in
-                HStack {
-                    ForEach(0..<4) { _ in
-                        Text("check")
-                    }
-                }
+            ForEach(0..<multipliers.count, id: \.self) { i in
+                MultiplierRow(multipliersForRow: multipliers[i])
             }
         }
     }
 }
 
+struct MultiplierRow: View {
+    var multipliersForRow: [Int]
+    var body: some View {
+        HStack {
+            ForEach(0..<multipliersForRow.count, id: \.self) { num in
+                Text("\(multipliersForRow[num])")
+            }
+        }
+    }
+}
 #Preview {
     StartView()
 }
